79822864fe812068861fe27d513fe9e67cdcf02b
kemi-p <kpole@dvtsoftware.com>
2026-04-09 15:08:27 +0200

    format the balance, add a close button for the card

--- src/app/components/account-details/account-details.ts
+++ src/app/components/account-details/account-details.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, EventEmitter, Output,Input  } from '@angular/core';
 
 
 @Component({
-  selector: 'app-root',
-  imports: [],
-  template: '',
-  styles: ''
+  selector: 'app-account-details',
+  imports: [CurrencyPipe],
+  template: `
+        <div class="card">
+           
+
+      <p BalanceHighlight> <!--TOdo: My directive to hightlith the bank balance-->
+        Balance: {{ balance | currency }}
+      </p>
+
+      <button (click)="close()">Close</button>
+
+
+        </div>
+  `,
+  styles: `
+  `
 })
-export class AccountDetails {}
+export class AccountDetails {
+
+    @Input({ required: true }) balance!: number;
+    @Output() closed: EventEmitter<any> = new EventEmitter();
+
+      close(){
+        this.closed.emit()
+      }
+}

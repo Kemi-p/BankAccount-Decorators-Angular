454cf03089a643f454a9ca8e7fa3ecd92cfd01aa
kemi-p <kpole@dvtsoftware.com>
2026-04-09 14:23:37 +0200

    initial files of angular bank account decorators exercise

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,12 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('BankAccount-Decorators-Angular');
+
+  accounts = [
+    {id:1, name:'Cheque', balance:1756},
+     {id:2, name:'Savings', balance:1756},
+      {id:3, name:'Fixed deposit', balance:1756},
+       {id:4, name:'Credit', balance:1756},
+        {id:5, name:'Business', balance:1756}
+  ]
 }

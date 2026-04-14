9ab6ca95c711ef3b8a4105d356f5c11d65404439
kemi-p <kpole@dvtsoftware.com>
2026-04-14 10:13:36 +0200

    add components for loan office, and for client and login

---
+++ src/app/components/client/client-portal.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core"
+
+@Component({
+    selector:'login',
+    imports:[],
+    templateUrl:'./client-portal.html',
+    styleUrl:'./client-portal.css'
+})
+
+export class ClientPortalComponent{
+
+}
---
+++ src/app/components/loan/loan-office.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core"
+
+@Component({
+    selector:'login',
+    imports:[],
+    templateUrl:'./loan-office.html',
+    styleUrl:'./loan-office.css'
+})
+
+export class LoanOfficelComponent{
+
+}

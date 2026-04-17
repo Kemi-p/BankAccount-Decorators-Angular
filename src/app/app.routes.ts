9aa9482e59a6441c522bdd8ebc481bd992757e58
kemi-p <kpole@dvtsoftware.com>
2026-04-17 14:32:07 +0200

    add a client role guard to restrict banker accessing the banker as a banker, have to login in a as client to create a loan request

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,12 @@ import { LoanOfficelComponent } from './components/loan/loan-office';
 import { App } from './app';
 import { BankComponent } from './bank/bank';
 import { LoginComponent } from './login/login';
-import { bankeGuard } from './role-guard';
+import { bankeGuard } from './routeGuards/role-guard';
+import { clientGuard } from './routeGuards/clientRole-guard';
 
 export const routes: Routes = [
     {path:'home', component: App },
-    {path:'client', component:ClientPortalComponent},
+    {path:'client', component:ClientPortalComponent, canActivate:[clientGuard]},
     {path:'loan', component: LoanOfficelComponent, canActivate: [bankeGuard]},
     {path:'bank', component: BankComponent },
     {path: 'login', component:LoginComponent},
--- src/app/routeGuards/clientRole-guard.ts
+++ src/app/routeGuards/clientRole-guard.ts
@@ -2,16 +2,16 @@ import { CanActivateFn } from '@angular/router';
 import { inject, Inject } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Router } from '@angular/router';
-import { selectUser } from './state/app/app.rselector';
+import { selectUser } from '../state/app/app.rselector';
 import { map } from 'rxjs/operators';
 
-export const bankeGuard: CanActivateFn = () => {
+export const clientGuard: CanActivateFn = () => {
   const store = inject(Store)
   const router = inject(Router)
 
    return store.select(selectUser).pipe(
     map(user => {
-      if (user?.role === 'banker') return true;
+      if (user?.role === 'client') return true;
 
       router.navigate(['/login']);
       return false;
